4432e11e8ed1f0f2a0ab318a631a791958c00b5c
VENOME <giveawayskeleton3@gmail.com>
2026-01-23 15:05:45 +0600

    Create config.js

---
+++ src/config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  nicknameChannelId: "YOUR_CHANNEL_ID",
+
+  ogRoleIds: [
+    "ROLE_ID_1", "ROLE_ID_2", "ROLE_ID_3", "ROLE_ID_4",
+    "ROLE_ID_5", "ROLE_ID_6", "ROLE_ID_7", "ROLE_ID_8",
+    "ROLE_ID_9", "ROLE_ID_10", "ROLE_ID_11", "ROLE_ID_12",
+    "ROLE_ID_13", "ROLE_ID_14", "ROLE_ID_15", "ROLE_ID_16",
+    "ROLE_ID_17", "ROLE_ID_18", "ROLE_ID_19", "ROLE_ID_20"
+  ],
+
+  resetKeyword: "vn reset",
+  maxNicknameLength: 32,
+  noPermissionMessage: "You must have the OG role to change your nickname.",
+  bannedWords: ["badword1", "badword2"]
+};
