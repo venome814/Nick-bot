c94e630af89e5a21f7e8b4ec97d44769f2909e0b
VENOME <giveawayskeleton3@gmail.com>
2026-01-23 15:06:27 +0600

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,27 @@
+require("dotenv").config();
+const { Client, GatewayIntentBits, Partials } = require("discord.js");
+const fs = require("fs");
+const path = require("path");
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+    GatewayIntentBits.GuildMembers
+  ],
+  partials: [Partials.Channel]
+});
+
+// Load events
+const eventsPath = path.join(__dirname, "events");
+fs.readdirSync(eventsPath).forEach(file => {
+  const event = require(path.join(eventsPath, file));
+  if (event.once) {
+    client.once(event.name, (...args) => event.execute(...args));
+  } else {
+    client.on(event.name, (...args) => event.execute(...args));
+  }
+});
+
+client.login(process.env.TOKEN);
